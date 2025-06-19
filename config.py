235898f2679a454b2b1538547e5b03c1901831f5
ariqlubis <ariqf.lubis@gmail.com>
2025-06-19 07:32:43 +0700

    change config

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 # Search configuration
+
+## Keywords
 KEYWORDS = [
-    "Prabowo"
+    "Prabowo",
+    # "Gibran"
 ]
 
 # Date range for search
@@ -15,10 +18,10 @@ MAX_SCROLLS = 5  # Maximum scrolls per keyword
 SCROLL_DELAY = 3  # Seconds between scrolls
 NO_NEW_TWEETS_LIMIT = 3  # Stop after this many scrolls with no new tweets
 
-# Browser options
+# Browser options [DONT CHANGE IF YOU DONT UNDERTAND, SAL]
 HEADLESS = False  # Set to True to run browser in headless mode
 MAXIMIZED = True  # Start browser maximized
 
-# Output settings
+# Output settings [YOU CAN CHANGE THIS IF YOU WANT]
 OUTPUT_FILENAME = "twitter_data"  # Base filename (timestamp will be added)
 OUTPUT_FORMAT = "csv"  # Options: "csv", "json", "excel"
